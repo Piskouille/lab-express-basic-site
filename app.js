76fd5f0d8ab9159c425dc2380e150d6934389570
Quentin Marignier <qmarignier@gmail.com>
2021-07-20 00:46:41 +0200

    fooled around with axios and img API

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const express = require("express")
 const axios = require('axios')
 const hbs = require("hbs")
-const pictures = require('./pictures')
-
+const authKey = process.env.UNSPLASH_API_AUTH
+const secretKey = process.env.UNSPLASH_API_SECRET_KEY
 const app = express()
 
 app.set('view engine', 'hbs')
@@ -11,14 +11,55 @@ app.use(express.static(__dirname + "/public"))
 hbs.registerPartials(__dirname + '/views/partials')
 
 
-const picturesDog = []
-for(let i = 0; i < 10; i++){
-    axios.get("https://dog.ceo/api/breeds/image/random")
-        .then(res => {
-            // console.log(res.data)
-            picturesDog.push({src: res.data.message, name: res.data.status})
-        })
+
+let pictures = []
+ const fetchUnsplash = async () => {
+    const response = await axios.get("https://api.unsplash.com/photos/random", {
+        headers: {
+            Authorization: `Client-ID PifXiRuHpGmHdGDEEnuXPrq1RTLkWKcUdTUPcGYHiVk` 
+        },
+        params: {
+            orientation: "squarish",
+            query: "landscape",
+            count: 25,
+            w: 576,
+            h: 576, 
+            fit: "crop",
+            dpr: 2
+        }
+    })  
+
+    pictures = response.data.map(res => {
+        return {
+            url: res.urls.regular, 
+            author: res.user.name}
+    }) 
 }
+
+
+fetchUnsplash()  
+
+/* const fetchPixabay = async () => {
+    const response = await axios.get("https://pixabay.com/api/" ,{
+        params : {
+            key: "22568889-ee2675af8f619367c0d472043",
+            q: "landscape",
+            image_type: "photo",
+            orientation: "horizontal",
+            min_width: "400px",
+            per_page: 10
+        }
+    })
+    
+    pictures = response.data.hits.map(res => {
+        return {
+            url: res.userImageURL, 
+            name: res.tags.split(', ')[0]
+        }
+    })
+}
+
+fetchPixabay() */
 
 
 app.get('/', (req, res, next) => {
@@ -31,11 +72,11 @@ app.get('/about', (req, res, next) => {
         css: ["/styles/main.css"]
     })
 })
-app.get('/gallery', (req, res, next) => {
+ app.get('/gallery', (req, res, next) => {
     res.render("gallery.hbs", {
         css: ["/styles/main.css", "/styles/pictures.css"],
-        pictures: picturesDog
+        pictures: pictures
     })
-})
+}) 
 
 app.listen(8080, () => console.log("Server is listening"))
